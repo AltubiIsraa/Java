584a97051ad198c6cb5c40b48857c757e2c9532f
altubiisraa <israaaltubi@gmail.com>
2022-02-06 13:28:04 +0400

    test

---
+++ src/main/java/hs/hs/HsApplication.java
@@ -0,0 +1,14 @@
+package hs.hs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsApplication.class, args);
+	}
+	
+}
